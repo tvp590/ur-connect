e021c9c31b19d96345d104b4c4679ceb03275b27
dev <devansh.sw.dev@gmail.com>
2022-07-06 23:30:39 -0600

    Added: Profile buttons and  message button redirects to chat app

--- src/ProfilePage/Pic.js
+++ src/ProfilePage/Pic.js
@@ -2,8 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import Header from "../components/Header";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { useNavigate } from "react-router-dom";
 
 function Pic() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/chat");
+  };
+
   return (
     <Wrap>
       <Header />
@@ -12,10 +19,12 @@ function Pic() {
         <BioContainer>
           <User__name>
             <h1>User__name</h1>
-            <p>
-              <strong>Follow</strong>
-            </p>
-            <MoreHorizIcon />
+            <ButtonsInProfile>Follow</ButtonsInProfile>
+
+            <ButtonsInProfile onClick={handleClick}>Message</ButtonsInProfile>
+            <ButtonsInProfile>
+              <MoreHorizIcon />
+            </ButtonsInProfile>
           </User__name>
           <AccountInfo>
             <p>
@@ -76,12 +85,11 @@ const User__name = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: center;
+
   h1 {
     font-weight: normal;
-  }
-  p {
-    margin-left: 40px;
-    margin-right: 10px;
+    margin-right: 20px;
   }
 `;
 
@@ -108,6 +116,17 @@ const Bio = styled.p``;
 
 const User__info = styled.div`
   line-height: 1.5;
+`;
+
+const ButtonsInProfile = styled.button`
+  font-weight: bold;
+  font-size: 14px;
+  margin-left: 10px;
+  border: none;
+  background: none;
+  cursor: pointer;
+  outline: none;
+  display: flex;
 `;
 
 export default Pic;
